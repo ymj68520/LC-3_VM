0ebc3c33b2b3a65adadf3c5a293bbe455a1afc44
ymj68520 <x68520@gmail.com>
2023-06-15 16:31:05 +0800

    Add Some operations.

--- Operations.cpp
+++ Operations.cpp
@@ -5,6 +5,7 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
 	}
 	return x;
 }
+
 void update_flags(uint16_t r) {
 	if (reg[r] == 0) {
 		reg[R_COND] = FL_ZRO;
@@ -18,7 +19,8 @@ void update_flags(uint16_t r) {
 		reg[R_COND] = FL_POS;
 	}
 }
-void ADD(uint16_t instr, int bit_count) {
+
+void ADD(uint16_t instr) {
 	uint16_t r0 = (instr >> 9) & 0x7; // rd
 	uint16_t r1 = (instr >> 6) & 0x7; // rs
 	uint16_t imm_flag = (instr >> 5) & 0x1; // it is a flag as you can see
@@ -33,4 +35,116 @@ void ADD(uint16_t instr, int bit_count) {
 		reg[r0] = reg[r1] + reg[r2];
 	}
 	update_flags(r0);
+}
+
+void LDI(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7; // destination register (DR)
+	uint16_t pc_offset = sign_extend(instr & 0x1ff, 9); // PCoffset 9
+
+	// add pc_offset to the current PC, look at that memory location to get the final address
+	reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+	update_flags(r0);
+}
+
+void AND(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t r1 = (instr >> 6) & 0x7;
+	uint16_t imm_flag = (instr >> 5) & 0x1;
+
+	if (imm_flag) {
+		uint16_t immSigned = sign_extend(instr & 0x1F, 5);
+		reg[0] = reg[1] & immSigned;
+	}
+	else
+	{
+		uint16_t r2 = instr & 0x7;
+		reg[r0] = reg[r1] & reg[r2];
+	}
+	update_flags(r0);
+}
+
+void NOT(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t r1 = (instr >> 6) & 0x7;
+
+	reg[r0] = ~reg[r1];
+	update_flags(r0);
+}
+
+void BR(uint16_t instr)
+{
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	uint16_t cond_flag = (instr >> 9) & 0x7;
+	if (cond_flag & reg[R_COND]) {
+		reg[R_PC] += pc_offset;
+	}
+}
+
+void JMP(uint16_t instr)
+{
+	uint16_t r1 = (instr >> 6) & 0x7;
+	reg[R_PC] = reg[r1];
+}
+
+void JSR(uint16_t instr)
+{
+	uint16_t long_flag = (instr >> 11) & 1;
+	reg[R_R7] = reg[R_PC];
+	if (long_flag) {
+		uint16_t long_pc_offset = sign_extend(instr & 0x7FF, 11);
+		reg[R_PC] += long_pc_offset;
+	}
+	else
+	{
+		uint16_t r1 = (instr >> 6) & 0x7;
+		reg[R_PC] = reg[r1];	// JSRR
+	}
+}
+
+void LD(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t r1 = (instr >> 6) & 0x7;
+	uint16_t offset = sign_extend(instr & 0x3FF, 6);
+	reg[r0] = mem_read(reg[r1] + offset);
+	update_flags(r0);
+}
+
+void LEA(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	reg[r0] = reg[R_PC] + pc_offset;
+	update_flags(r0);
+}
+
+void ST(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	mem_write(reg[R_PC] + pc_offset, reg[r0]);
+
+}
+
+void STI(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	mem_write(mem_read(reg[R_PC] + pc_offset), reg[r0]);
+}
+
+void STR(uint16_t instr)
+{
+	uint16_t r0 = (instr >> 9) & 0x7;
+	uint16_t r1 = (instr >> 6) & 0x7;
+	uint16_t offset = sign_extend(instr & 0x3F, 6);
+	mem_write(reg[r1] + offset, reg[r0]);
+}
+
+void BDC()
+{
+	abort();
 }
--- Operations.h
+++ Operations.h
@@ -2,11 +2,37 @@
 #include "types.h"
 #include "registers.h"
 #include "condition_flag.h"
+#include "LoadingPrograms.h"
+#include "memOpts.h"
+#include <stdlib.h>
 // let integer be its TC, just say it is sign extend
 uint16_t sign_extend(uint16_t x, int bit_count);
 // update flags as CPU compute result
 void update_flags(uint16_t r);
 // add operations
-void ADD(uint16_t instr, int bit_count);
-// C LDI Sample
+void ADD(uint16_t instr);
+// LDI
+void LDI(uint16_t instr);
+// Bitwise AND
+void AND(uint16_t instr);
+// Bitwise NOT
+void NOT(uint16_t instr);
+// Branch
+void BR (uint16_t instr);
+// Jump
+void JMP(uint16_t instr);
+// Jump register
+void JSR(uint16_t instr);
+// Load
+void LD (uint16_t instr);
+// Load effective address
+void LEA(uint16_t instr);
+// Store
+void ST (uint16_t instr);
+// Store indirect
+void STI(uint16_t instr);
+// Store register
+void STR(uint16_t instr);
+// Bad Code
+void BDC();
 
